845b07bd01936456a795f2224ac13620ba886391
Aditya K Kamath <akamath1997@gmail.com>
2020-12-25 17:08:31 +0530

    Add indices for matrices

--- PartA/header/single_thread.h
+++ PartA/header/single_thread.h
@@ -1,15 +1,29 @@
 // Optimize this function
 void singleThread(int N, int *matA, int *matB, int *output)
 {
-    // Iterate over output elements
-    for(int i = 0; i < 2 * N - 1; ++i) {
+    // Iterate over first half of output elements
+    for(int i = 0; i < N; ++i) {
         int temp = 0;
         // Iterate over diagonal elements
-        for(int j = 0; j < min(i + 1, 2 * N - (i + 1)); ++j) {
-            int rowA = 0;
-            int colA = 0;
-            int rowB = 0;
-            int colB = 0;
+        for(int j = 0; j < i + 1; ++j) {
+            int rowA = j;
+            int colA = i - j;
+            int rowB = i - j;
+            int colB = N - j - 1;
+            temp += matA[rowA * N + colA] * matB[rowB * N + colB];
+        }
+        output[i] = temp;
+    }
+    
+    // Iterate over second half of output elements
+    for(int i = N; i < 2 * N - 1; ++i) {
+        int temp = 0;
+        // Iterate over diagonal elements
+        for(int j = 0; j < 2 * N - (i + 1); ++j) {
+            int rowA = i + 1 + j - N;
+            int colA = N - j - 1;
+            int rowB = N - j - 1;
+            int colB = 2 * N - j - 2 - i;
             temp += matA[rowA * N + colA] * matB[rowB * N + colB];
         }
         output[i] = temp;
