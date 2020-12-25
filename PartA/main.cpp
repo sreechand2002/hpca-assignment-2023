7b01425efda4af98bf54cd829462921d4d74a02f
Aditya K Kamath <akamath1997@gmail.com>
2020-12-25 17:54:42 +0530

    Add reference program, and untimed execution

--- PartA/main.cpp
+++ PartA/main.cpp
@@ -6,8 +6,43 @@
 #include <fstream>
 using namespace std;
 
+#define TIME_NOW std::chrono::high_resolution_clock::now()
+#define TIME_DIFF(gran, start, end) std::chrono::duration_cast<gran>(end - start).count()
+
 #include "single_thread.h"
 #include "multi_thread.h"
+
+// Used to cross-check answer. DO NOT MODIFY!
+void reference(int N, int *matA, int *matB, int *output)
+{
+    // Iterate over first half of output elements
+    for(int i = 0; i < N; ++i) {
+        int temp = 0;
+        // Iterate over diagonal elements
+        for(int j = 0; j < i + 1; ++j) {
+            int rowA = j;
+            int colA = i - j;
+            int rowB = i - j;
+            int colB = N - j - 1;
+            temp += matA[rowA * N + colA] * matB[rowB * N + colB];
+        }
+        output[i] = temp;
+    }
+    
+    // Iterate over second half of output elements
+    for(int i = N; i < 2 * N - 1; ++i) {
+        int temp = 0;
+        // Iterate over diagonal elements
+        for(int j = 0; j < 2 * N - (i + 1); ++j) {
+            int rowA = i + 1 + j - N;
+            int colA = N - j - 1;
+            int rowB = N - j - 1;
+            int colB = 2 * N - j - 2 - i;
+            temp += matA[rowA * N + colA] * matB[rowB * N + colB];
+        }
+        output[i] = temp;
+    }
+}
 
 int main()
 {
@@ -27,26 +62,40 @@ int main()
     for(int i = 0; i < N; ++i)
         for(int j = 0; j < N; ++j)
             cin >> matB[i * N + j];
-
-    clock_t begin, end; 
+    
+    // Untimed, warmup caches and TLB
+    int *output_reference = new int[2 * N - 1];
+    reference(N, matA, matB, output_reference);    
+    
+    // Execute reference program
+    auto begin = TIME_NOW;
+    reference(N, matA, matB, output_reference);
+    auto end = TIME_NOW;
+    cout << "Reference execution time: " << (double)TIME_DIFF(std::chrono::microseconds, begin, end) / 1000.0 << " ms\n";    
     
     // Execute single thread
     int *output_single = new int[2 * N - 1];
-    begin = clock();
+    begin = TIME_NOW;
     singleThread(N, matA, matB, output_single);
-    end = clock();
-    cout << "Single thread execution time: " << double(end - begin) / (double)CLOCKS_PER_SEC << " seconds\n";
+    end = TIME_NOW;
+    cout << "Single thread execution time: " << (double)TIME_DIFF(std::chrono::microseconds, begin, end) / 1000.0 << " ms\n";
+    
+    for(int i = 0; i < 2 * N - 1; ++i)
+        if(output_single[i] != output_reference[i]) {
+            cout << "Mismatch at " << i << "\n";
+            exit(1);
+        }
     
     // Execute multi-thread
     int *output_multi = new int[2 * N - 1];
-    begin = clock();
+    begin = TIME_NOW;
     multiThread(N, matA, matB, output_multi);
-    end = clock();
-    cout << "Multi-threaded execution time: " << double(end - begin) / (double)CLOCKS_PER_SEC << " seconds\n";
+    end = TIME_NOW;
+    cout << "Multi-threaded execution time: " << (double)TIME_DIFF(std::chrono::microseconds, begin, end) / 1000.0 << " ms\n";
     
     for(int i = 0; i < 2 * N - 1; ++i)
-        if(output_single[i] != output_multi[i]) {
-            printf("Mismatch at %d\n", i);
+        if(output_multi[i] != output_reference[i]) {
+            cout << "Mismatch at " << i << "\n";
             exit(1);
         }
 }
