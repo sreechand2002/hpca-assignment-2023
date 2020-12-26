3062d1862e2ea82f36437a07a3072b6b1f827af9
Shweta Pandey <shwetapandey@iisc.ac.in>
2020-12-26 10:11:22 +0530

    Generator function writes directly to the input file

--- PartA/data/generate.cpp
+++ PartA/data/generate.cpp
@@ -1,29 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-#define N 8192
-
-int main()
+int main(int argc, char *argv[])
 {
+    int N; 
+    if (argc < 2) 
+        N = 8192;
+    else 
+        N = atoi(argv[1]); 
+    ofstream ofile; 
+    string file_name = "data/input_" + to_string(N) + ".in";  
+    ofile.open(file_name); 
+
     // Set seed
     srand(1);
     
     // Output square matrix size
-    cout << N << "\n";
-    
+    ofile << N << "\n"; 
     // Generate matrix A
     for(int i = 0; i < N; ++i) {
         for(int j = 0; j < N; ++j)
-            cout << rand() % 256 << " ";
-        cout << "\n";
+            ofile << rand() % 256 << " ";
+        ofile << "\n";
     }
     
     // Generate matrix B
     for(int i = 0; i < N; ++i) {
         for(int j = 0; j < N; ++j)
-            cout << rand() % 256 << " ";
-        cout << "\n";
+            ofile << rand() % 256 << " ";
+        ofile << "\n";
     }
 }
