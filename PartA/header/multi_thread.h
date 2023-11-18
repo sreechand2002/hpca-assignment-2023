83653d07f4e562c9112f521ebfb8a478c652dbf8
sreechand2002 <sreechand2002@gmail.com>
2023-11-18 23:27:59 +0530

    PARTA

--- PartA/header/multi_thread.h
+++ PartA/header/multi_thread.h
@@ -3,16 +3,60 @@
 // Create other necessary functions here
 
 
-// Fill in this function
-void multiThread( int input_row, 
-                int input_col,
-                int *input, 
-                int kernel_row, 
-                int kernel_col, 
-                int *kernel,
-                int output_row, 
-                int output_col, 
-                long long unsigned int *output ) 
-{
+#include <omp.h>
 
+void multiThread(int input_row,
+                 int input_col,
+                 int *input,
+                 int kernel_row,
+                 int kernel_col,
+                 int *kernel,
+                 int output_row,
+                 int output_col,
+                 long long unsigned int *output) {
+
+    int *inputPtr = input;
+    long long unsigned int *outputPtr = output;
+
+    // Parallelize the outer loop using OpenMP
+    #pragma omp parallel for collapse(2)
+    for (int output_i = 0; output_i < output_row; output_i++) {
+        for (int output_j = 0; output_j < output_col; output_j++) {
+            long long unsigned int outputValue = 0;
+
+            for (int kernel_i = 0; kernel_i < kernel_row; kernel_i++) {
+                for (int kernel_j = 0; kernel_j < kernel_col; kernel_j++) {
+                    int input_i = (output_i + 2 * kernel_i) % input_row;
+                    int input_j = (output_j + 2 * kernel_j) % input_col;
+                    outputValue += inputPtr[input_i * input_col + input_j] * kernel[kernel_i * kernel_col + kernel_j];
+                }
+            }
+
+            // Use reduction to avoid race conditions on the shared output array
+            #pragma omp critical
+            outputPtr[output_i * output_col + output_j] = outputValue;
+        }
+    }
 }
+
+
+
+
+// Fill in this function
+// void multiThread( int input_row, 
+//                 int input_col,
+//                 int *input, 
+//                 int kernel_row, 
+//                 int kernel_col, 
+//                 int *kernel,
+//                 int output_row, 
+//                 int output_col, 
+//                 long long unsigned int *output ) 
+// {
+
+
+    
+// }
+
+
+
--- PartA/header/single_thread.h
+++ PartA/header/single_thread.h
@@ -1,32 +1,36 @@
 // Optimize this function
-void singleThread( int input_row, 
-                int input_col,
-                int *input, 
-                int kernel_row, 
-                int kernel_col, 
-                int *kernel,
-                int output_row, 
-                int output_col, 
-                long long unsigned int *output ) 
-{
-    
-    for(int i = 0; i < output_row * output_col; ++i)
-        output[i] = 0;
 
-    for(int output_i = 0; output_i< output_row; output_i++)
-    {
-        for(int output_j = 0; output_j< output_col; output_j++)
-        {
-            for(int kernel_i = 0; kernel_i< kernel_row; kernel_i++)
-            {
-                for(int kernel_j = 0; kernel_j< kernel_col; kernel_j++)
-                {
-                    int input_i = (output_i + 2*kernel_i) % input_row;
-                    int input_j = (output_j + 2*kernel_j) % input_col;
-                    output[output_i * output_col + output_j] += input[input_i*input_col +input_j] 
-                                                                * kernel[kernel_i*kernel_col +kernel_j];
+void singleThread(register int input_row,
+                  register int input_col,
+                  register int *input,
+                  register int kernel_row,
+                  register int kernel_col,
+                  register int *kernel,
+                  register int output_row,
+                  register int output_col,
+                  register long long unsigned int *output) {
+
+    register int input_i,input_j,val1,val2;
+    int *inputPtr = input;
+    long long unsigned int *outputPtr = output,outputValue=0;
+
+    for (int output_i = 0; output_i < output_row; output_i++) {
+        for (int output_j = 0; output_j < output_col; output_j++) {
+            outputValue = 0;
+            for (int kernel_i = 0; kernel_i < kernel_row; kernel_i++) {
+                val1 = output_i + (kernel_i+kernel_i);
+                input_i = val1 - input_row * (val1/input_row);
+                for (int kernel_j = 0; kernel_j < kernel_col; kernel_j++) {
+
+                    
+                    val2 = output_j + (kernel_j+kernel_j);
+                    input_j = val2 - input_col*(val2/input_col);
+
+                    
+                    outputValue += inputPtr[input_i * input_col + input_j] * kernel[kernel_i * kernel_col + kernel_j];
                 }
             }
+            outputPtr[output_i * output_col + output_j] = outputValue;
         }
     }
 }
